b7e95a2e69d9225b0ca230b8049e1f91c9814ab3
Jomithius <jomithius.swassa@gmail.com>
2025-08-18 11:54:26 -0500

    pressing B stop learning move
    instead of looping back to asking to relearn/learn/teach

--- src/party_menu.c
+++ src/party_menu.c
@@ -5774,6 +5774,9 @@ static void Task_HandleStopLearningMoveYesNoInput(u8 taskId)
 
     switch (Menu_ProcessInputNoWrapClearOnChoose())
     {
+    case MENU_B_PRESSED:
+       PlaySE(SE_SELECT);
+       // fallthrough
     case 0:
         GetMonNickname(mon, gStringVar1);
         StringCopy(gStringVar2, GetMoveName(gPartyMenu.data1));
@@ -5790,9 +5793,6 @@ static void Task_HandleStopLearningMoveYesNoInput(u8 taskId)
             gTasks[taskId].func = Task_ClosePartyMenuAfterText;
         }
         break;
-    case MENU_B_PRESSED:
-        PlaySE(SE_SELECT);
-        // fallthrough
     case 1:
         GetMonNickname(mon, gStringVar1);
         StringCopy(gStringVar2, GetMoveName(gPartyMenu.data1));
