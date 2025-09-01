77ec559bb6e397a63bc1c155a090256b231f005f
Jomithius <jomithius.swassa@gmail.com>
2025-09-01 04:46:38 -0500

    changes to new mauville side quest
    made new mauville required to progress
    increased levels of new mauvile mons

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -106,7 +106,7 @@
 #define VAR_PACIFIDLOG_TOWN_STATE                        0x4056 // Unused Var
 #define VAR_PETALBURG_CITY_STATE                         0x4057
 #define VAR_SLATEPORT_CITY_STATE                         0x4058
-#define VAR_MAUVILLE_CITY_STATE                          0x4059 // Unused Var
+#define VAR_MAUVILLE_CITY_STATE                          0x4059 // Used for new mauville sidequest, custom. 0-2 start new mauville, 3 end of mauville
 #define VAR_RUSTBORO_CITY_STATE                          0x405A
 #define VAR_FORTREE_CITY_STATE                           0x405B // Unused Var
 #define VAR_LILYCOVE_CITY_STATE                          0x405C // Unused Var
