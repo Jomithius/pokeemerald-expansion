442ecf0e2ae4033a96ec5678aeda1412f9c05fcf
Jomithius <jomithius.swassa@gmail.com>
2025-11-01 04:02:26 -0500

    sprite adjustments
    adjustments to typhlosion and meganium sprite

--- src/data/items.h
+++ src/data/items.h
@@ -14559,7 +14559,7 @@ const struct Item gItemsInfo[] =
         .holdEffect = HOLD_EFFECT_MEGA_STONE,
         .description = COMPOUND_STRING(
             "This stone enables\n"
-            "Malamite to Mega\n"
+            "Malamar to Mega\n"
             "Evolve in battle."),
         .pocket = POCKET_ITEMS,
         .type = ITEM_USE_BAG_MENU,
